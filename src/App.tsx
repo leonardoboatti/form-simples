67ea892286644c71ed9f68bd9730f8367ab268e8
Leonardo Boatti <leonardo.boatti@yakult.com.br>
2022-04-13 14:40:46 -0300

    ex form simples

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
+import React, {useState} from "react";
+
 const App = () => {
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [idade, setIdade] = useState('');
+
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>)=> {
+    setName(event.target.value);
+    
+  }
+
+  const handleInput1 = (event: React.ChangeEvent<HTMLInputElement>)=> {
+    setLastName(event.target.value);
+    
+  }
+
+  const handleInput2 = (event: React.ChangeEvent<HTMLInputElement>)=> {
+    setIdade(event.target.value);
+    
+  }
+
+
   return (
     <div>
       <div>
         Nome:
-        <input type="text" />
+        <input type="text"  value={name} onChange={handleInput}/>
       </div>
 
       <div>
         Sobrenome:
-        <input type="text" />
+        <input type="text" value={lastName} onChange={handleInput1}/>
       </div>
 
       <div>
         Idade:
-        <input type="text" />
+        <input type="text" value={idade} onChange={handleInput2}/>
       </div>
 
       <hr/>
 
-      Olá ---, tudo bem?<br/>
-      Você tem -- anos.
+      Olá {name} {lastName}, tudo bem?<br/>
+      Você tem {idade} anos.
     </div>
   );
 }
